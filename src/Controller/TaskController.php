c81464c40affd7df0a374dd50ba5311114859692
Romain Lentz <romain.lentz74@gmail.com>
2023-10-26 16:34:06 +0200

    [ADD] formulaire d'ajout d'une tache

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Entity\Task;
+use App\Form\TaskType;
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,9 +25,21 @@ class TaskController extends AbstractController
 
 
     #[Route('/task/{toDoListId}/add', name: 'app_task')]
-    public function addTask(int $toDoListId): Response
+    public function addTask(int $toDoListId, CategoryRepository $categoryRepository, Request $request): Response
     {
-//      dd($request->getSession()->get("config"));
-        return $this->render('task/add_task.html.twig');
+        $task = new Task();
+        $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            dd($form->getData());
+            $task = $form->getData();
+
+            return $this->redirectToRoute('app_to_do_list');
+        }
+        return $this->render('task/add_task.html.twig', [
+            'form' => $form
+        ]);
     }
 }
---
+++ src/Form/TaskType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Task;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use SebastianBergmann\CodeCoverage\Report\Text;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Nom de la tâche',
+                'required' => true,
+                'error_bubbling' => true,
+
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Déscription de la tâche',
+                'required'=>false,
+            ])
+            ->add('expirationDate', DateTimeType::class, [
+                'label' => 'Date d\'éxpiration',
+            ])
+            ->add('categories', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Créer'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
